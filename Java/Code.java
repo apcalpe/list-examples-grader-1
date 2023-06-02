8f8c8ecc7d17d6a5d1fa70ce4242e5076d6ca268
apcalpe <130112293+apcalpe@users.noreply.github.com>
2023-06-02 09:03:04 -0700

    edits

---
+++ Java/Code.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+public class Code {
+   public static void main(String[] args) {
+       File file = new File(args[0]);
+       if (file.exists()) {
+           System.out.println(file); 
+           System.out.println("File exists, yay!");
+       }
+       else {
+            System.out.println(file); 
+           System.out.println("The file does not exist :(");
+       }
+   }
+}
